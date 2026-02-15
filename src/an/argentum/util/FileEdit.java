bc7134746766cfa15324145f3c6fafdbb371ba02
Silver Fairies <nikita666.wern@gmail.com>
2026-02-15 19:47:29 +0100

    File deletion & Pair

--- src/an/argentum/util/FileEdit.java
+++ src/an/argentum/util/FileEdit.java
@@ -1,5 +1,20 @@
 package an.argentum.util;
 
+import java.io.File;
+import java.nio.file.Path;
+
 public final class FileEdit {
-    
+    public static void delete ( Path path, String directory ) {
+        Path buffer = path.resolve(directory);
+        for ( File each : buffer.toFile().listFiles()) delete(each);
+    }
+
+    private static void delete ( File file ) {
+        if ( !file.isDirectory() || (file.isDirectory() && file.listFiles().length == 0) ) file.delete(); else {
+            for(File each : file.listFiles()) {
+                delete(each);
+            }
+            file.delete();
+        }
+    }
 }
---
+++ src/an/argentum/util/Pair.java
@@ -0,0 +1,19 @@
+package an.argentum.util;
+
+public class Pair <T> {
+    private T key;
+    private T value;
+
+    public Pair ( T key, T value ) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public T getKey () {
+        return this.key;
+    }
+
+    public T getValue () {
+        return this.value;
+    }
+}
